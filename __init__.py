1d5e780c12d1835d4e4c771ab1431bd0c1e933a6
jmawanzi <165791421+jmawanzi@users.noreply.github.com>
2024-04-11 10:40:47 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
-@app.route('/')
+@app.route('/contact/')
 def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
+    return "<h2>Ma page de contact</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
 @app.route('/exercices/')
 def exercices():
